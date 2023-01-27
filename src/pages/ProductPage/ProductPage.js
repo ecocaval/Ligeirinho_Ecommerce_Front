5a63d5bb09d0721ba07249933d8cef4ccc5577fa
erico <ecocaval@hotmail.com>
2023-01-27 18:52:58 -0300

    Adicionando mais responsividade e textarea na tela de produto

--- src/pages/ProductPage/ProductPage.js
+++ src/pages/ProductPage/ProductPage.js
@@ -5,6 +5,7 @@ import styled from "styled-components"
 import { useEffect, useState } from "react";
 import axios from "axios";
 import ImageGallery from 'react-image-gallery';
+import { BsFillCheckCircleFill } from "react-icons/bs";
 import "react-image-gallery/styles/css/image-gallery.css";
 
 
@@ -13,12 +14,25 @@ export default function ProductPage() {
     const [productRequested, setProductRequested] = useState({})
     const [productImages, setProductImages] = useState([])
     const [restaurantRequested, setRestaurantRequested] = useState({})
+    const [productQuantity, setProductQuantity] = useState(1)
     const [receivedInfo, setReceivedInfo] = useState(false)
+    const [userWantsDescription, setUserWantsDescription] = useState(false)
 
+    //! Temporario - debug 
     const exampleUrl = "http://localhost:5000/restaurants/63d30f2494d5e0d0a25d2c99/products/63d3129594d5e0d0a25d2ca9"
     const exampleToken = "Bearer 84101767-d4fc-451f-b0e2-73d6a546573c"
     const restaurantUrl = "http://localhost:5000/restaurants"
 
+    //* Estilo do Icon ao lado da descrição quando ativo
+    const checkerStyle = {
+        transition: "all 0.2s",
+        boxShadow: "0px 0px 15px 0px #9aeb98",
+        borderRadius: "12px",
+
+        ":hover": {
+            cursor: "pointer"
+        }
+    }
 
     useEffect(() => async () => {
 
@@ -27,9 +41,7 @@ export default function ProductPage() {
                 authorization: exampleToken
             }
         })
-
         const product = getResponse.data
-
         const images = product.bigImages.map(image => ({
             original: image
         }))
@@ -39,66 +51,104 @@ export default function ProductPage() {
                 authorization: exampleToken
             }
         })
-
         const restaurant = getResponse.data
 
         setProductImages(images)
         setRestaurantRequested(restaurant)
         setProductRequested(product)
         setReceivedInfo(true)
-
     }, [])
 
     return (
-        <>
+        <ProductPageStyle>
             <Header />
             <Wrapper>
                 <StyledMain>
                     <ProductWrapper>
                         <ProductFlex>
                             <ProductTitle>
-                                {receivedInfo ? "Macarrão com Queijo" : "Carregando..."}
+                                {receivedInfo ? productRequested.name : "Carregando..."}
                             </ProductTitle>
                             <RestaurantInfo>
                                 {receivedInfo ?
                                     <>
-                                        <img src={restaurantRequested.smallImages} alt=""/>
+                                        <img src={restaurantRequested.smallImages} alt="" />
                                         <p>{restaurantRequested.name}</p>
                                     </> :
                                     "Carregando..."
                                 }
                             </RestaurantInfo>
                         </ProductFlex>
-
                         <ProductImages>
                             <ImageGallery
                                 items={productImages}
+                                showFullscreenButton={false}
+                                showPlayButton={false}
+                                autoPlay={true}
+                                slideInterval={5000}
                             />
                         </ProductImages>
                         <ProductDescription>
                             {receivedInfo ? productRequested.description : "Carregando..."}
                         </ProductDescription>
                     </ProductWrapper>
+
+                    <DivisionLine />
+
                     <BuyWrapper>
-                        <ProductPrice>
-
-                        </ProductPrice>
+                        <BackgroundPrice>
+                            <ProductPrice>
+                                R$ {receivedInfo ? String((productRequested.price * productQuantity).toFixed(2)) : "Carregando..."}
+                            </ProductPrice>
+                        </BackgroundPrice>
                         <ProductQuantity>
-
+                            <AddButton
+                                onClick={() => {
+                                    if (productQuantity >= 10) return
+                                    const quantity = productQuantity + 1
+                                    setProductQuantity(quantity)
+                                }}
+                            >+</AddButton>
+                            <QuantityTextContainer>
+                                <QuantityText>{productQuantity}</QuantityText>
+                            </QuantityTextContainer>
+                            <SubtractButton
+                                onClick={() => {
+                                    if (productQuantity <= 1) return
+                                    const quantity = productQuantity - 1
+                                    setProductQuantity(quantity)
+                                }}
+                            >-</SubtractButton>
                         </ProductQuantity>
                     </BuyWrapper>
                     <DescriptionWrapper>
-                        <DescriptionTitle></DescriptionTitle>
-                        <DescriptionInput />
+                        <DescriptionTitle>
+                            <p>Deseja adicionar alguma observação?</p>
+                            <BsFillCheckCircleFill
+                                onClick={() => setUserWantsDescription(!userWantsDescription)}
+                                color={userWantsDescription && "#30cf2d"}
+                                style={userWantsDescription ? checkerStyle : {
+                                    transition: "all 0.2s"
+                                }}
+                            />
+                        </DescriptionTitle>
+                        <DescriptionInput
+                            disabled={!userWantsDescription}
+                            userWantsDescription={userWantsDescription}
+                            type="text"
+                            placeholder={userWantsDescription && "Escreva sua descrição..."}
+                        />
                     </DescriptionWrapper>
-
-
                 </StyledMain>
             </Wrapper>
             <Footer />
-        </>
+        </ProductPageStyle >
     )
 }
+
+const ProductPageStyle = styled.main`
+    font-family: 'Barlow Condensed';
+`
 
 const ProductFlex = styled.div`
     position: relative;
@@ -106,17 +156,15 @@ const ProductFlex = styled.div`
     max-width: 600px;
     height: 50px;
     margin: 30px auto;
-    /* border: 2px blue solid; */
 `
 
 const StyledMain = styled.main`
     margin: auto;
-    /* background-color: blue; */
     position: fixed;
-    margin-top: 140px;
     width: 100%;
-    height: 100%;
+    height: calc(100% - 140px);
     z-index: 1;
+    padding-bottom: 30px;
     overflow-y: scroll;
 `
 
@@ -128,7 +176,6 @@ const Wrapper = styled.div`
 `
 
 const ProductWrapper = styled.section`
-    font-family: 'Barlow Condensed';
 `
 
 const ProductTitle = styled.div`
@@ -161,7 +208,6 @@ const RestaurantInfo = styled.div`
     transform: translate(-50%, -50%);
     transition: all 0.3s;
     opacity: 1;
-    /* border: 2px solid green; */
 
     @media (max-width: 320px) {
         opacity: 0;
@@ -182,11 +228,8 @@ const RestaurantInfo = styled.div`
 `
 
 const ProductImages = styled.figure`
-    /* height: 80%; */
-    /* border: 2px green solid; */
     max-width: 600px;
     margin: 10px auto;
-    /* margin: 0 30px; */
     overflow: hidden;
     border: 2px solid #ff8d00;
     border-radius: 10px;
@@ -199,7 +242,6 @@ const ProductImages = styled.figure`
     img {
         max-width: 400px;
         height: 230px;
-        /* border: 2px blue solid; */
     }
 `
 
@@ -207,35 +249,170 @@ const ProductDescription = styled.p`
     font-family: 'Barlow Condensed';
     font-size: 20px;
     max-width: 600px;
-    max-height: 120px;
     margin: 20px auto;
     text-align: center;
     padding: 0px 20px;
-    overflow: hidden; //! Fix later, should not remove the text
     text-overflow: ellipsis;
+`
+
+const DivisionLine = styled.section`
+    border: 1px solid #ff8d00;
+    margin: 20px auto;
 `
 
 const BuyWrapper = styled.section`
     border: 2px solid #ff8d00;
+    border-radius: 20px;
+    width: 50%;
+    max-width: 250px;
+    margin: auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    @media (max-width: 450px) { 
+        flex-direction: column;
+        min-width: 120px;
+        /* height: 100px; */
+    }
 `
 
-const ProductPrice = styled.div`
+const BackgroundPrice = styled.div`
+    width: fit-content;
+    padding: 0px 10px;
+    border-radius: 20px 0px 0px 20px;
+    height: 40px;
+    background-color: lightgray;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: all 0.5;
 
+    @media (max-width: 450px) { 
+        width: 100%;
+        border-radius: 20px 20px 0px 0px;
+    }
+`
+
+const ProductPriceContainer = styled.div`
+`
+
+const ProductPrice = styled.p`
+    text-align: center;
+    font-size: 20px;
 `
 
 const ProductQuantity = styled.div`
+    display: flex;
+    align-items: center;
+    margin-right: 15%;
+    transition: all 0.3s;
 
+    @media (max-width: 450px) { 
+        margin: 20px 0;
+    }
+`
+
+const AddButton = styled.button`
+    width: 30px;
+    height: 30px;
+    border-radius: 15px;
+    border: none;
+    background-color: #18a33d;
+    color: white;
+    font-size: 20px;
+    font-weight: bold;
+    margin-right: -5px;
+    z-index: 2;
+    transition: all 0.2s linear;
+
+    &:hover {
+        box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.8); 
+        cursor: pointer; 
+    }
+`
+
+const QuantityTextContainer = styled.div`
+    width: 40px;
+    height: 25px;
+    border: 1.5px solid lightgray;
+    box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.5);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+const QuantityText = styled.p`
+    font-size: 19px;
+`
+
+const SubtractButton = styled.button`
+    text-align: center;
+    width: 30px;
+    height: 30px;
+    border-radius: 15px;
+    border: none;
+    background-color: #a3183b;
+    color: white;
+    font-size: 20px;
+    font-weight: bold;
+    margin-left: -5px;
+    z-index: 2;
+
+    &:hover {
+        box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.8); 
+        cursor: pointer; 
+    }
 `
 
 const DescriptionWrapper = styled.section`
-
+    display: flex;
+    flex-direction: column;
+    width: auto;
 `
 
-const DescriptionTitle = styled.p`
+const DescriptionTitle = styled.div`
+    margin: 20px auto;
+    font-size: 22px;
+    color: gray;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    transition: all 0.5s;
 
+    @media (max-width: 320px) {
+        flex-direction: column;
+        gap: 15px;
+        max-width: 180px;
+    }
+
+    p {
+        margin-right: 10px;
+
+        @media (max-width: 320px) {
+            margin-right: 0;
+        }
+    }
 `
 
-const DescriptionInput = styled.input`
-    display: none;
+const DescriptionInput = styled.textarea`
+    font-family: 'Barlow Condensed';
+    white-space: pre-wrap; 
+    font-size: 20px;
+    padding: 15px;
+    width: 50%;
+    min-width: 250px;
+    max-width: 600px;
+    height: ${props => props.userWantsDescription ? "200px" : "0px"};
+    margin: auto;
+    transition: all 0.2s ease-out ;
+    border: ${props => props.userWantsDescription ? "1px lightgray solid;" : "none"};
+    box-shadow: ${props => props.userWantsDescription ? "0px 0px 10px 0px rgba(0, 0, 0, 0.5)" : "none"};
+    background-color: #ffffff;
+    transition: all 0.3s;
+
+    @media (max-width: 270px) {
+        min-width: 80%;
+    }
 `
 
